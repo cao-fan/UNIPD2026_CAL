b040aa69fca6eab489cf94f62ab14f9337f7ab38
cao_fan <95587916+cao-fan@users.noreply.github.com>
2026-01-11 18:15:52 +0100

    Update script.jl

--- script.jl
+++ script.jl
@@ -5,6 +5,7 @@ using Manifolds, LinearAlgebra, LieGroups
 using Enzyme
 using StaticArrays
 
+@info "Loading the data..."
 pcs,scan_coords = CylinderCalibration.load_2d_scans("scans10");
 T_EB_data,x_data,shuffled_pcs = CylinderCalibration.prepare_data(pcs,scan_coords; threshold=1.0);
 
@@ -25,11 +26,18 @@ r = 25.0 / 2
 cylinder = vcat(v,c,r)
 
 # Visualize initial initialization
+@info "Preparing visualization"
 CylinderCalibration.visualize_calibration_ex(T_EB_data, x_data, T_CE, cylinder, shuffled_pcs)
 
 # Calibrate
 data = (T_EB_data,x_data,cylinder,T_CE);
+@info "Setting up the solver..."
 opt_T_CE, opt_cylinder = CylinderCalibration.solve(data; max_iters=50)
 
 # Visualize calibration results
+@info "Showing calibration results"
+println("Optimal camera pose:")
+display(opt_T_CE)
+println("Fitted cylinder:")
+display(opt_cylinder)
 CylinderCalibration.visualize_calibration_ex(T_EB_data, x_data, opt_T_CE, opt_cylinder,shuffled_pcs)
